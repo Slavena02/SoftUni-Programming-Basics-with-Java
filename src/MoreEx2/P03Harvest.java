f353cae1f2e8d9273216fe46793e009afdffa79c
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-05-09 22:11:51 +0300

    Changes

--- src/MoreEx2/P03Harvest.java
+++ src/MoreEx2/P03Harvest.java
@@ -1,5 +1,33 @@
 package MoreEx2;
 
+import java.util.Scanner;
+
 public class P03Harvest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int areaTheVineyard = Integer.parseInt(scanner.nextLine());
+        double grapeOneSquareMeter = Double.parseDouble(scanner.nextLine());
+        int neededLitresWine = Integer.parseInt(scanner.nextLine());
+        int workers = Integer.parseInt(scanner.nextLine());
+
+
+        double totalGrape =  areaTheVineyard * grapeOneSquareMeter;
+
+        double totalWine = 0.4 * totalGrape  / 2.5;
+
+        if (totalWine < neededLitresWine){
+            System.out.printf("It will be a tough winter! More %.0f liters wine needed." , neededLitresWine - totalWine);
+        }
+        if (totalWine >= neededLitresWine ){
+
+            double excessWine = totalWine - neededLitresWine;
+
+            System.out.printf("Good harvest this year! Total wine: %.0f liters." , totalWine);
+            System.out.printf("%.0f liters left -> %.0f liters per person." , excessWine, excessWine / workers);
+        }
+
+
+    }
 
 }
