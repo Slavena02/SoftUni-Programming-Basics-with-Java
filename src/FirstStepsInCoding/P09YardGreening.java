97fb3c5f8b5909e788696dd91251d99ffe926b76
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-05-12 00:12:53 +0300

    Changes

--- src/FirstStepsInCoding/P09YardGreening.java
+++ src/FirstStepsInCoding/P09YardGreening.java
@@ -7,9 +7,9 @@ public class P09YardGreening {
         Scanner scanner = new Scanner(System.in);
         double yardMetres = Double.parseDouble(scanner.nextLine());
 
-        double fullSumMet = yardMetres * 7.61;
-        double discountSum = fullSumMet - (fullSumMet * 0.18);
-        double totalSum = fullSumMet  - discountSum;
+        double fullSumMetres = yardMetres * 7.61;
+        double discountSum = fullSumMetres - (fullSumMetres * 0.18);
+        double totalSum = fullSumMetres  - discountSum;
 
         System.out.printf("The final price is: %.2f lv.%n The discount is:%.2f lv."  , discountSum , totalSum);
     }
---
+++ src/ProgrammingBasicsOnlineExam/P03CoffeeMachine.java
@@ -0,0 +1,15 @@
+package ProgrammingBasicsOnlineExam;
+
+import java.util.Scanner;
+
+public class P03CoffeeMachine {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String name = scanner.nextLine();
+        String condition = scanner.nextLine();
+        int n = Integer.parseInt(scanner.nextLine());
+
+
+    }
+}
