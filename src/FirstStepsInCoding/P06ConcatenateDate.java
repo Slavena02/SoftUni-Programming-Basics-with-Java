9914073c1ff003fe7c952f7bcba42775e39aa398
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-05-12 01:14:57 +0300

    Changes

--- src/FirstStepsInCoding/P06ConcatenateDate.java
+++ src/FirstStepsInCoding/P06ConcatenateDate.java
@@ -6,11 +6,11 @@ public class P06ConcatenateDate {
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in);
 
-        String fName = scanner.nextLine();
-        String lName = scanner.nextLine();
+        String firstName = scanner.nextLine();
+        String lastName = scanner.nextLine();
         int age  = Integer.parseInt(scanner.nextLine());
         String town  = scanner.nextLine();
 
-        System.out.printf("You are %s %s, a %d-years old person from %s." , fName , lName , age , town);
+        System.out.printf("You are %s %s, a %d-years old person from %s." , firstName , lastName , age , town);
     }
 }
