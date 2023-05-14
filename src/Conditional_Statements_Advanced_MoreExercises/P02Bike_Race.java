13e4ffb7dcb7af5c8720735069ce6fe3a5d4490a
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-05-14 18:22:34 +0300

    Changes

---
+++ src/Conditional_Statements_Advanced_MoreExercises/P02Bike_Race.java
@@ -0,0 +1,18 @@
+package Conditional_Statements_Advanced_MoreExercises;
+
+import java.util.Scanner;
+
+public class P02Bike_Race {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner((System.in));
+
+        int juniorRacer = Integer.parseInt(scanner.nextLine());
+        int seniorRacer = Integer.parseInt(scanner.nextLine());
+        String route = scanner.nextLine();
+
+        String people = " ";
+        double feePrice = 0.00;
+
+
+    }
+}
