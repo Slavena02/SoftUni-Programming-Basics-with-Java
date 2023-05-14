fb1491322f08363af0bfb4196f8e6174f56974a2
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-05-14 23:12:29 +0300

    Changes

--- src/Conditional_Statements_Advanced_MoreExercises/P02Bike_Race.java
+++ src/Conditional_Statements_Advanced_MoreExercises/P02Bike_Race.java
@@ -10,8 +10,56 @@ public class P02Bike_Race {
         int seniorRacer = Integer.parseInt(scanner.nextLine());
         String route = scanner.nextLine();
 
-        String people = " ";
-        double feePrice = 0.00;
+        double feePriceJunior = 0.00;
+        double feePriceSenior = 0.00;
+
+        switch (route){
+
+            case "trail":
+                feePriceJunior = juniorRacer * 5.50;
+                feePriceSenior = seniorRacer * 7.00;
+
+                double totalFeeTrail = feePriceJunior + feePriceSenior;
+
+                double totalSumTrail = totalFeeTrail * 0.95;
+;
+                System.out.printf("%.2f" , totalSumTrail);
+                break;
+            case "cross-country":
+                feePriceJunior = juniorRacer * 8.00;
+                feePriceSenior = seniorRacer * 9.50;
+
+                double totalFeeCrossCountry = feePriceJunior + feePriceSenior;
+
+                int racers = juniorRacer + seniorRacer ;
+                if (racers >= 50){
+                    totalFeeCrossCountry = totalFeeCrossCountry * 0.75;
+                }
+                double totalSumCrossCountry = totalFeeCrossCountry * 0.95;
+
+                System.out.printf("%.2f" , totalSumCrossCountry);
+                break;
+            case "downhill":
+                feePriceJunior = juniorRacer * 12.25;
+                feePriceSenior = seniorRacer * 13.75;
+
+                double totalFeeDownhill = feePriceJunior + feePriceSenior;
+
+                double totalSumDownhill = totalFeeDownhill * 0.95;
+
+                System.out.printf("%.2f" , totalSumDownhill);
+                break;
+            case "road":
+                feePriceJunior = juniorRacer * 20.00;
+                feePriceSenior = seniorRacer * 21.50;
+
+                double totalFeeRoad = feePriceJunior + feePriceSenior;
+
+                double totalSumRoad = totalFeeRoad * 0.95;
+
+                System.out.printf("%.2f",totalSumRoad);
+        }
+
 
 
     }
