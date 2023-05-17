c656973873567cab9008c61fd70ce3d9977f928d
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-05-17 22:55:47 +0300

    Changes

--- src/ProgrammingBasicsOnlineExam_1/P01BirthdayParty.java
+++ src/ProgrammingBasicsOnlineExam_1/P01BirthdayParty.java
@@ -1,5 +1,23 @@
 package ProgrammingBasicsOnlineExam_1;
 
+import java.util.Scanner;
+
 public class P01BirthdayParty {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double budget = Double.parseDouble(scanner.nextLine());
+
+        double cake = budget  * 0.20;
+        double drinks = cake - (cake * 0.45);
+        double anime =  budget / 3;
+
+        double totalSum = budget + cake + drinks + anime;
+
+        System.out.printf("%.2f" , totalSum);
+
+
+
+    }
 
 }
