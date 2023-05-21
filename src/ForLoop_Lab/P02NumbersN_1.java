3c35fa793ad7e4640a03d931e97f6bb5b3ff4932
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-05-21 23:49:02 +0300

    add new project

--- src/ForLoop_Lab/P02NumbersN_1.java
+++ src/ForLoop_Lab/P02NumbersN_1.java
@@ -1,4 +1,16 @@
 package ForLoop_Lab;
 
+import java.util.Scanner;
+
 public class P02NumbersN_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = Integer.parseInt(scanner.nextLine());
+
+        for (int num = number; num>=1 ; num--){
+            System.out.println(num);
+
+        }
+    }
 }
