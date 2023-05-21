eeb99ce9ae6e36186bea6a59a7197b748376b3d4
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-05-21 23:54:21 +0300

    add new project

--- src/ForLoop_Lab/P04EvenPowersOf2.java
+++ src/ForLoop_Lab/P04EvenPowersOf2.java
@@ -5,6 +5,12 @@ import java.util.Scanner;
 public class P04EvenPowersOf2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int n  = Integer.parseInt(scanner.nextLine());
+
+        for (int power = 0;power<=n; power+=2){
+            System.out.println(Math.pow(2,power));
+
+        }
 
 
     }
