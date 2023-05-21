f93880809f64cd34b2bc055bb4a484c6c0d48d0f
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-05-22 00:46:55 +0300

    change

---
+++ src/ForLoop_Lab/P06VowelsSum.java
@@ -0,0 +1,13 @@
+package ForLoop_Lab;
+
+import java.util.Scanner;
+
+public class P06VowelsSum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String text = scanner.nextLine();
+
+
+    }
+}
