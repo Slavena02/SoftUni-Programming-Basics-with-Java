0d35054d6e64283ec8b3d71292ba980aa1dfae03
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-05-22 00:00:40 +0300

    change

--- src/ForLoop_Lab/P05CharacterSequence.java
+++ src/ForLoop_Lab/P05CharacterSequence.java
@@ -1,4 +1,16 @@
 package ForLoop_Lab;
 
+import java.util.Scanner;
+
 public class P05CharacterSequence {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String text = scanner.nextLine();
+
+        for (int position = 0; position <=text.length()-1; position++){
+            System.out.println(text.charAt(position));
+
+        }
+    }
 }
