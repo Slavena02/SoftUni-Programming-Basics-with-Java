e45ea191399159f116c6f763844e0e7094a6a2e4
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-05-26 22:05:29 +0300

    change

--- src/ForLoop_Lab/P08NumberSequence.java
+++ src/ForLoop_Lab/P08NumberSequence.java
@@ -1,4 +1,11 @@
 package ForLoop_Lab;
 
+import java.util.Scanner;
+
 public class P08NumberSequence {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner((System.in));
+
+
+    }
 }
