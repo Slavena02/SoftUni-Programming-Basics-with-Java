6129f95352344c1c9c765fc23b86c2fe07304919
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-05-28 12:11:35 +0300

    change

--- src/ForLoop_Lab/P08NumberSequence.java
+++ src/ForLoop_Lab/P08NumberSequence.java
@@ -6,6 +6,25 @@ public class P08NumberSequence {
     public static void main(String[] args) {
         Scanner scanner = new Scanner((System.in));
 
+        int n  = Integer.parseInt(scanner.nextLine());
+
+        int big = Integer.MIN_VALUE;
+        int small  = Integer.MAX_VALUE;
+
+        for (int i = 1; i <= n ; i++) {
+
+            int num = Integer.parseInt(scanner.nextLine());
+
+            if (num < small){
+                small = num;
+            }
+            if (num > big){
+                big = num;
+            }
+        }
+
+        System.out.printf("Max number: %d%n", big);
+        System.out.printf("Min number: %d",small);
 
     }
 }
