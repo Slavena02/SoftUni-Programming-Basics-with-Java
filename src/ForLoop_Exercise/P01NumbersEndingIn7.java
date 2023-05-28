9deb0905e746fa9ee84a924049fc33842a67d2cb
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-05-28 13:22:12 +0300

    add

--- src/ForLoop_Exercise/P01NumbersEndingIn7.java
+++ src/ForLoop_Exercise/P01NumbersEndingIn7.java
@@ -1,4 +1,13 @@
 package ForLoop_Exercise;
 
 public class P01NumbersEndingIn7 {
+    public static void main(String[] args) {
+
+
+        for (int i = 1; i < 1000; i++) {
+            if (i % 10 == 7) {
+            System.out.println(i);
+        }
+    }
 }
+    }
