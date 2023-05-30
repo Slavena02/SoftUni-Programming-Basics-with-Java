76c6f6f228a8081f6a86f5377291a1081d73f06b
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-05-31 00:54:46 +0300

    add

---
+++ src/ForLoop_Exercise/P06Oscars.java
@@ -0,0 +1,18 @@
+package ForLoop_Exercise;
+
+import java.util.Scanner;
+
+public class P06Oscars {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String nameActor = scanner.nextLine();;
+        double pointsAcademy = Double.parseDouble(scanner.nextLine());
+        int jury = Integer.parseInt(scanner.nextLine());
+
+
+        for (int i = 0; i < ; i++) {
+            
+        }
+    }
+}
