ddc39684bc08e0707b6de547e6fd9b2389d97751
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-05-31 00:36:34 +0300

    add

--- src/ForLoop_Exercise/P05Salary.java
+++ src/ForLoop_Exercise/P05Salary.java
@@ -5,9 +5,33 @@ import java.util.Scanner;
 public class P05Salary {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int tab = Integer.parseInt(scanner.nextLine());
+
+        int tabN = Integer.parseInt(scanner.nextLine());
         double salary = Double.parseDouble(scanner.nextLine());
 
+
+        for (int i = 1; i <= tabN; i++) {
+            String openTab = scanner.nextLine();
+            switch (openTab) {
+                case "Facebook":
+                    salary -= 150.00;
+                    break;
+                case "Instagram":
+                    salary -= 100.00;
+                    break;
+                case "Reddit":
+                    salary -= 50.00;
+                    break;
+            }
+            if (salary <= 0) {
+                System.out.println("You have lost your salary.");
+                break;
+            }
+        }
+
+        if (salary > 0) {
+            System.out.printf("%.0f", salary);
+        }
     }
 
 }
