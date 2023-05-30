297e5c7be1b65d4db53ccefe6dda74f5b487d9f6
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-05-30 23:55:33 +0300

    add

---
+++ src/ForLoop_Exercise/P05Salary.java
@@ -0,0 +1,13 @@
+package ForLoop_Exercise;
+
+import java.util.Scanner;
+
+public class P05Salary {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int tab = Integer.parseInt(scanner.nextLine());
+        double salary = Double.parseDouble(scanner.nextLine());
+
+    }
+
+}
