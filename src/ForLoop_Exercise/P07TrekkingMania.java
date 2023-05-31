7d51a01f6d652140f68f88ee659f9e9c1d38c14a
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-05-31 23:12:49 +0300

    add

---
+++ src/ForLoop_Exercise/P07TrekkingMania.java
@@ -0,0 +1,11 @@
+package ForLoop_Exercise;
+
+import java.util.Scanner;
+
+public class P07TrekkingMania {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
