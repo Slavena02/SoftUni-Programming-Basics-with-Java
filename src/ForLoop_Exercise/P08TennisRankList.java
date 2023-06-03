471b607d345cf8edc528eedb4a41271fc2682909
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-03 22:18:25 +0300

    add

--- src/ForLoop_Exercise/P08TennisRankList.java
+++ src/ForLoop_Exercise/P08TennisRankList.java
@@ -7,5 +7,39 @@ public class P08TennisRankList {
         Scanner scanner =new Scanner(System.in);
 
 
+        int tournaments = Integer.parseInt(scanner.nextLine());
+        int startPoints = Integer.parseInt(scanner.nextLine());
+
+        int wens = 0;
+
+        int pointW = 2000;
+        int pointF = 1200;
+        int pointSF = 720;
+        int sum = startPoints;
+
+        for (int i = 1; i <= tournaments ; i++) {
+            String stage = scanner.nextLine();
+
+            if (stage.equals("W")) {
+                sum += pointW;
+                wens++;
+            } else if (stage.equals("F")) {
+                sum += pointF;
+            } else if (stage.equals("SF")) {
+                sum += pointSF;
+            }
+        }
+
+
+
+        int totalSum = sum - startPoints;
+
+            double arithmeticMean = Math.floor(totalSum * 1.0 / tournaments);
+            double percentWens = (wens * 1.0 / tournaments) * 100;
+
+        System.out.printf("Final points: %d%n" ,sum);
+        System.out.printf("Average points: %.0f%n",arithmeticMean );
+        System.out.printf("%.2f%%%n" ,percentWens);
+
     }
 }
