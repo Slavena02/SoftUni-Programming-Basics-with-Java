341bcf0e9c04cd106155f7b7e555b22922f86744
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-03 17:37:37 +0300

    add

--- src/ForLoop_Exercise/P08TennisRankList.java
+++ src/ForLoop_Exercise/P08TennisRankList.java
@@ -1,4 +1,10 @@
 package ForLoop_Exercise;
 
+import java.util.Scanner;
+
 public class P08TennisRankList {
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+
+    }
 }
