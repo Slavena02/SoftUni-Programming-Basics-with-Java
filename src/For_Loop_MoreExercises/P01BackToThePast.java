72a4c10a870bd9223a3ba183332660e845ff02a4
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-03 23:21:59 +0300

    add

--- src/For_Loop_MoreExercises/P01BackToThePast.java
+++ src/For_Loop_MoreExercises/P01BackToThePast.java
@@ -4,7 +4,29 @@ import java.util.Scanner;
 
 public class P01BackToThePast {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner (System.in);
+        Scanner scanner = new Scanner(System.in);
 
+        double budget = Double.parseDouble(scanner.nextLine());
+        int years = Integer.parseInt(scanner.nextLine());
+
+        int age = 18;
+
+        double sum = 0;
+
+        for (int i = 1800; i <= years; i++) {
+
+            if (i % 2 == 0) {
+                sum = budget - 12000;
+            } else {
+                ++age;
+                sum = budget - (12000 + age * 50);
+            }
+        }
+
+        if (budget > 0){
+            System.out.printf("Yes! He will live a carefree life and will have %.2f dollars left." , sum);
+        }else {
+            System.out.printf("He will need %.2f dollars to survive.", Math.abs(sum) );
+        }
     }
 }
