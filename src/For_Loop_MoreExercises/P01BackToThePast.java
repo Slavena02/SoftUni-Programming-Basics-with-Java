b38c539b580c4b0d29c0e4fa882979565ca512b1
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-03 23:51:51 +0300

    add

--- src/For_Loop_MoreExercises/P01BackToThePast.java
+++ src/For_Loop_MoreExercises/P01BackToThePast.java
@@ -11,22 +11,20 @@ public class P01BackToThePast {
 
         int age = 18;
 
-        double sum = 0;
-
         for (int i = 1800; i <= years; i++) {
 
             if (i % 2 == 0) {
-                sum = budget - 12000;
+                budget -= 12000;
             } else {
                 ++age;
-                sum = budget - (12000 + age * 50);
+                budget -= 12000 + age * 50;
             }
         }
 
         if (budget > 0){
-            System.out.printf("Yes! He will live a carefree life and will have %.2f dollars left." , sum);
+            System.out.printf("Yes! He will live a carefree life and will have %.2f dollars left." , budget);
         }else {
-            System.out.printf("He will need %.2f dollars to survive.", Math.abs(sum) );
+            System.out.printf("He will need %.2f dollars to survive.", Math.abs(budget));
         }
     }
 }
--- src/For_Loop_MoreExercises/P02Hospital.java
+++ src/For_Loop_MoreExercises/P02Hospital.java
@@ -1,4 +1,26 @@
 package For_Loop_MoreExercises;
 
+import java.util.Scanner;
+
 public class P02Hospital {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int period = Integer.parseInt(scanner.nextLine());
+
+        int doctors = 7;
+        int treatedPatients = 0;
+        int untreatedPatients = 0;
+
+        for (int i = 1; i <= period ; i++) {
+            int patient = Integer.parseInt(scanner.nextLine());
+
+            if (doctors == patient){
+                treatedPatients += doctors;
+            }else{
+                untreatedPatients -= doctors ;
+            }
+
+        }
+    }
 }
