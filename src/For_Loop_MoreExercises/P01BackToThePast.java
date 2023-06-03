2ac7da856c0cdc57a5f6521ab44a00e117eccbc2
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-03 22:22:21 +0300

    add

---
+++ src/For_Loop_MoreExercises/P01BackToThePast.java
@@ -0,0 +1,10 @@
+package For_Loop_MoreExercises;
+
+import java.util.Scanner;
+
+public class P01BackToThePast {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+
+    }
+}
