f621beabb654e0c44e6634979f8ac6035c337eba
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-04 16:55:17 +0300

    add

---
+++ src/WhileLoop_Lab/P01ReadText.java
@@ -0,0 +1,17 @@
+package WhileLoop_Lab;
+
+import java.util.Scanner;
+
+public class P01ReadText {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        while (!input.equals("Stop")){
+            System.out.println(input);
+            input = scanner.nextLine();
+        }
+
+    }
+}
