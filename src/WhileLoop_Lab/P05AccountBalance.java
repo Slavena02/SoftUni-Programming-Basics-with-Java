f8bb777d648713fd8d0fdbc3f7384b1fdf395e74
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-05 00:14:13 +0300

    add

--- src/WhileLoop_Lab/P05AccountBalance.java
+++ src/WhileLoop_Lab/P05AccountBalance.java
@@ -6,6 +6,15 @@ public class P05AccountBalance {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String input = scanner.nextLine();
 
-    }
+        double sum = 0;
+        while (!input.equals("NoMoreMoney")){
+            double many = Integer.parseInt(scanner.nextLine());
+            System.out.printf("Increase:%f " , many);
+            sum += many;
+        }
+        System.out.printf("Total: %f" , input);
+        }
+
 }
