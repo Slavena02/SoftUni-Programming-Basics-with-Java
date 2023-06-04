d2744162d7cac4d58e7c3802ac6de5c7d1a84728
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-04 17:04:03 +0300

    add

---
+++ src/WhileLoop_Lab/P04Sequence2K1.java
@@ -0,0 +1,10 @@
+package WhileLoop_Lab;
+
+import java.util.Scanner;
+
+public class P04Sequence2K1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
