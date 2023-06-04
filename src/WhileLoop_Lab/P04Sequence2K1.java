9cd6f0c032b3265b67899600a8cd0cb4f9cdc298
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-04 17:14:36 +0300

    add

--- src/WhileLoop_Lab/P04Sequence2K1.java
+++ src/WhileLoop_Lab/P04Sequence2K1.java
@@ -6,5 +6,12 @@ public class P04Sequence2K1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int n = Integer.parseInt(scanner.nextLine());
+
+        int input = 1;
+        while (input <= n) {
+            System.out.println(input);
+            input = input * 2 + 1;
+        }
     }
 }
