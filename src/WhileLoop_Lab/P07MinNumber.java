95bc5add4bb1f9fd9925e047d556fda4fa3a6eae
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-07 01:44:09 +0300

    add

---
+++ src/WhileLoop_Lab/P07MinNumber.java
@@ -0,0 +1,11 @@
+package WhileLoop_Lab;
+
+import java.util.Scanner;
+
+public class P07MinNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
