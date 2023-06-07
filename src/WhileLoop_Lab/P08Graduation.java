7a9786aaa86b93ebfb07903f08b24522d1a522cd
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-07 22:09:34 +0300

    add

--- src/WhileLoop_Lab/P08Graduation.java
+++ src/WhileLoop_Lab/P08Graduation.java
@@ -1,4 +1,10 @@
 package WhileLoop_Lab;
 
+import java.util.Scanner;
+
 public class P08Graduation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
 }
