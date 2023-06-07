bb93eb966df43b62ad9453def3169b4cb12c5040
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-07 22:17:19 +0300

    add

--- src/WhileLoop_Exercise/P01OldBooks.java
+++ src/WhileLoop_Exercise/P01OldBooks.java
@@ -1,4 +1,21 @@
 package WhileLoop_Exercise;
 
+import java.util.Scanner;
+
 public class P01OldBooks {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String book = scanner.nextLine();
+
+        int books = 0;
+        while (!book.equals("No More Books")){
+
+            System.out.println("The book you search is not here!");
+            System.out.printf("You checked {брой} books.");
+            book = scanner.nextLine();
+
+        }
+    }
 }
