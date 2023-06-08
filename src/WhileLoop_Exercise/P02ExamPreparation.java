3ba1cb14e2ce52b41e4f476c22771ac4d1142083
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-08 23:30:57 +0300

    add

--- src/WhileLoop_Exercise/P02ExamPreparation.java
+++ src/WhileLoop_Exercise/P02ExamPreparation.java
@@ -1,4 +1,21 @@
 package WhileLoop_Exercise;
 
+import java.util.Scanner;
+
 public class P02ExamPreparation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int lowEstimate  = Integer.parseInt(scanner.nextLine());
+
+        String input = scanner.nextLine();
+        int estimate = Integer.parseInt(scanner.nextLine());
+        int sum = 0;
+        int sumEstimate = 0;
+        while (!input.equals("Enough")){
+            sumEstimate ++;
+            sum += estimate;
+        }
+
+
+    }
 }
