8af4654133a50f7cbbfbfbbf2e3d58b5a38bdff0
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-10 01:31:43 +0300

    add

--- src/WhileLoop_Lab/P08Graduation.java
+++ src/WhileLoop_Lab/P08Graduation.java
@@ -5,13 +5,34 @@ import java.util.Scanner;
 public class P08Graduation {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-          String name = scanner.nextLine();
 
-          double sum = Double.parseDouble(scanner.nextLine());
+        String name = scanner.nextLine();
 
-          int ended = 0;
-          while (sum >=4 ){
-          }
-          ended++;
+        int year = 1;
+        double sumGrade = 0;
+        int poorGradeCount = 0;
+        while (year <= 12) {
+            if (poorGradeCount > 1) {
+                break;
+            }
+            double grade = Double.parseDouble(scanner.nextLine());
+
+            if (grade < 4) {
+                poorGradeCount++;
+                continue;
+            }
+
+            sumGrade = sumGrade + grade;
+
+            year++;
+        }
+
+        if (poorGradeCount > 1) {
+            System.out.printf("%s has been excluded at %d grade", name, year);
+        } else {
+            double avgGrade = sumGrade / 12;
+            System.out.printf("%s graduated. Average grade: %.2f", name, avgGrade);
+        }
     }
 }
+
