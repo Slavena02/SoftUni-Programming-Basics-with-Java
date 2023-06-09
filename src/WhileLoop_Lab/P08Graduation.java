847602c797f720f405a2a2575944f66283f2a77f
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-10 01:21:29 +0300

    add

--- src/WhileLoop_Lab/P08Graduation.java
+++ src/WhileLoop_Lab/P08Graduation.java
@@ -5,6 +5,13 @@ import java.util.Scanner;
 public class P08Graduation {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+          String name = scanner.nextLine();
 
+          double sum = Double.parseDouble(scanner.nextLine());
+
+          int ended = 0;
+          while (sum >=4 ){
+          }
+          ended++;
     }
 }
