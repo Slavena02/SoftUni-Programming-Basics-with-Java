55fb58718daaace872e66b29c9e1d2c9f8d9c722
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-11 02:01:15 +0300

    add

---
+++ src/Conditional_Statements_Advanced_MoreExercises/P06TruckDriver.java
@@ -0,0 +1,10 @@
+package Conditional_Statements_Advanced_MoreExercises;
+
+import java.util.Scanner;
+
+public class P06TruckDriver {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
