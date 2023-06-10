3bd5105931f297a559b2e8c806a24db85bea06de
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-11 02:52:51 +0300

    add

---
+++ src/Conditional_Statements_Advanced_MoreExercises/P08PointOnRectangleBorder.java
@@ -0,0 +1,10 @@
+package Conditional_Statements_Advanced_MoreExercises;
+
+import java.util.Scanner;
+
+public class P08PointOnRectangleBorder {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
