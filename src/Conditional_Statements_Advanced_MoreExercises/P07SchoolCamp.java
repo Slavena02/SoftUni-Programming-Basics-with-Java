5bf9ae3ab7d82ee300194b0a843099eddfa2546e
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-11 02:42:03 +0300

    add

--- src/Conditional_Statements_Advanced_MoreExercises/P07SchoolCamp.java
+++ src/Conditional_Statements_Advanced_MoreExercises/P07SchoolCamp.java
@@ -6,5 +6,69 @@ public class P07SchoolCamp {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String season = scanner.nextLine();
+        String groupType = scanner.nextLine();
+        int students = Integer.parseInt(scanner.nextLine());
+        int countNight = Integer.parseInt(scanner.nextLine());
+
+        String sport = " ";
+        double oneNightPrice = 0.00;
+
+        switch (season) {
+            case "Winter":
+                switch (groupType) {
+                    case "boys":
+                        sport = "Judo";
+                    case "girls":
+                        oneNightPrice = 9.60;
+                        sport = "Gymnastics";
+                        break;
+                    case "mixed":
+                        oneNightPrice = 10.00;
+                        sport = "Ski";
+                        break;
+                }
+                break;
+            case "Spring":
+                switch (groupType) {
+                    case "boys":
+                        sport = "Tennis";
+                    case "girls":
+                        oneNightPrice = 7.20;
+                        sport = "Athletics";
+                        break;
+                    case "mixed":
+                        oneNightPrice = 9.50;
+                        sport = "Cycling";
+                        break;
+                }
+                break;
+            case "Summer":
+                switch (groupType) {
+                    case "boys":
+                        sport = "Football";
+                    case "girls":
+                        oneNightPrice = 15.00;
+                        sport = "Volleyball";
+                        break;
+                    case "mixed":
+                        oneNightPrice = 20.00;
+                        sport = "Swimming";
+                        break;
+                }
+                break;
+        }
+        double totalSum = countNight * oneNightPrice;
+
+        if (students >= 50){
+            totalSum = totalSum - (totalSum * 0.50);
+        }else if (students >= 20){
+            totalSum = totalSum - (totalSum * 0.15);
+        }else  if (students >= 10){
+            totalSum = totalSum - (totalSum * 0.05);
+        }
+
+        System.out.printf("%s %.2f lv." , sport ,totalSum);
     }
+
 }
