612ce4e51f7ca0f9474ead9c390a97d395c1adbb
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-10 22:38:38 +0300

    add

--- src/ProgrammingBasicsOnlineExam_1/P03FilmPremiere.java
+++ src/ProgrammingBasicsOnlineExam_1/P03FilmPremiere.java
@@ -1,4 +1,10 @@
 package ProgrammingBasicsOnlineExam_1;
 
+import java.util.Scanner;
+
 public class P03FilmPremiere {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
 }
