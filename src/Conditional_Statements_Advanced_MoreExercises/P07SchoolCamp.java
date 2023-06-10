5e9a392f04fe807f1068165406d15ced00371c8d
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-11 02:17:57 +0300

    add

---
+++ src/Conditional_Statements_Advanced_MoreExercises/P07SchoolCamp.java
@@ -0,0 +1,10 @@
+package Conditional_Statements_Advanced_MoreExercises;
+
+import java.util.Scanner;
+
+public class P07SchoolCamp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
