1d35948f6f0c842a09dc678c2fe3245701d64a42
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-10 22:21:34 +0300

    add

--- src/Exams/P01Task.java
+++ src/Exams/P01Task.java
@@ -6,18 +6,21 @@ public class P01Task {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-
-        String name = scanner.nextLine();
+        String nameMovie = scanner.nextLine();
         int season = Integer.parseInt(scanner.nextLine());
-        int episode = Integer.parseInt(scanner.nextLine());
-        double teamEpisode = Double.parseDouble(scanner.nextLine());
+        int episodes = Integer.parseInt(scanner.nextLine());
+        double episodesFreeAdvertisement = Integer.parseInt(scanner.nextLine());
 
-        double advertisementEpisode = episode + (episode * 0.20);
-        double sumEpisode = episode + advertisementEpisode;
-        double specialEpisode = season * 10;
-        double totalSumMinutes = sumEpisode * episode * season + 30;
-        System.out.printf("Total time needed to watch the %s series is %.0f minutes.", name, totalSumMinutes);
+        int  extraTime = 10;
+        double advertisementTime = episodesFreeAdvertisement * 0.2;
 
+        double oneEpisode = episodesFreeAdvertisement + advertisementTime;
+
+        double extraTimeTotal = extraTime * season;
+
+        double totalSumTime = Math.floor(oneEpisode * episodes * season + extraTimeTotal);
+
+        System.out.printf("Total time needed to watch the %s series is %.0f minutes." , nameMovie, totalSumTime);
 
     }
 }
