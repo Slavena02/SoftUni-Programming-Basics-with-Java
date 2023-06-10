51cc2124b4b60be8cf736a4d062f47efa747d333
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-11 02:12:19 +0300

    add

--- src/Conditional_Statements_Advanced_MoreExercises/P06TruckDriver.java
+++ src/Conditional_Statements_Advanced_MoreExercises/P06TruckDriver.java
@@ -6,5 +6,40 @@ public class P06TruckDriver {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String season = scanner.nextLine();
+        double kilometersOneMonth = Double.parseDouble(scanner.nextLine());
+
+        double sumOneKilometers = 0.00;
+        double totalSum = 0.00;
+        switch (season){
+            case "Autumn":
+            case "Spring":
+                    if (kilometersOneMonth <= 5000){
+                        sumOneKilometers = 0.75;
+                    }else if (kilometersOneMonth <= 10000){
+                        sumOneKilometers = 0.95;
+                    }else if (kilometersOneMonth <= 20000){
+                        sumOneKilometers = 1.45;
+                    }
+                break;
+            case "Summer":
+                if (kilometersOneMonth <= 5000){
+                    sumOneKilometers = 0.90;
+                }else if (kilometersOneMonth <= 10000){
+                    sumOneKilometers = 1.10;
+                }else if (kilometersOneMonth <= 20000){
+                    sumOneKilometers = 1.45;
+                }
+                break;
+            case "Winter":
+                if (kilometersOneMonth <= 5000){
+                    sumOneKilometers = 1.05;
+                }else if (kilometersOneMonth <= 10000){
+                    sumOneKilometers = 1.25;
+                }else if (kilometersOneMonth <= 20000){
+                    sumOneKilometers = 1.45;
+                }
+                break;
+        }
     }
 }
