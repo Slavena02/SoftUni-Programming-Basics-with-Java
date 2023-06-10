01a07945724538d732681668e7de1cd8787ea4b8
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-11 01:09:30 +0300

    add

---
+++ src/Conditional_Statements_Advanced_MoreExercises/P04CarToGo.java
@@ -0,0 +1,10 @@
+package Conditional_Statements_Advanced_MoreExercises;
+
+import java.util.Scanner;
+
+public class P04CarToGo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
