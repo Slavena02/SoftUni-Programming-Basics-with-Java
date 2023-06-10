39f8b11b32d0211d7c914b5298807513e6ce8cfa
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-10 22:28:28 +0300

    add

--- src/ProgrammingBasicsOnlineExam_1/P03MovieDay.java
+++ src/ProgrammingBasicsOnlineExam_1/P03MovieDay.java
@@ -6,6 +6,11 @@ public class P03MovieDay {
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in);
 
+        double time = Double.parseDouble(scanner.nextLine());
+        int scenes = Integer.parseInt(scanner.nextLine());
+        int timeOfScenes = Integer.parseInt(scanner.nextLine());
+
+        double preparation = time * 0.15;
 
 
     }
