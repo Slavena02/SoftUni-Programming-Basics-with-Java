5705ea39fa99db100b49ad789ec8d21b00464ae5
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-10 22:36:45 +0300

    add

--- src/ProgrammingBasicsOnlineExam_1/P02CatWalking.java
+++ src/ProgrammingBasicsOnlineExam_1/P02CatWalking.java
@@ -22,4 +22,58 @@ public class P02CatWalking {
             System.out.printf("No, the walk for your cat is not enough. Burned calories per day: %.0f." ,caloriesDay);
         }
     }
+
+    public static class P01PoolDay {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+
+            int people = Integer.parseInt(scanner.nextLine());
+            double entranceFee = Double.parseDouble(scanner.nextLine());
+            double oneDayPriceSunBed = Double.parseDouble(scanner.nextLine());
+            double oneUmbrellaPrice = Double.parseDouble(scanner.nextLine());
+
+            double entranceFeePrice = people * entranceFee;
+
+            double peoplePercent =Math.ceil (people - (people * 0.25));
+            double totalSumSunBed = peoplePercent * oneDayPriceSunBed;
+
+            double umbrellaPercent = Math.ceil(people - (people * 0.50));
+
+            double totalSumUmbrella = umbrellaPercent * oneUmbrellaPrice;
+
+            double totalSumPoolDay = entranceFeePrice + totalSumSunBed + totalSumUmbrella;
+
+            System.out.printf("%.2f lv." , totalSumPoolDay);
+
+
+        }
+    }
+
+    public static class P02FamilyTrip {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            double budget = Double.parseDouble(scanner.nextLine());
+            int night = Integer.parseInt(scanner.nextLine());
+            double oneNightPrice = Double.parseDouble(scanner.nextLine());
+            double percent = Double.parseDouble(scanner.nextLine());
+
+
+
+            if (night > 7){
+                oneNightPrice = oneNightPrice - (oneNightPrice*0.05);
+            }
+            double nightPrice = night * oneNightPrice;
+
+            double percentPrice = budget * (percent/100);
+
+            double totalSum = nightPrice + percentPrice;
+
+            if (budget >= totalSum){
+               System.out.printf("Ivanovi will be left with %.2f leva after vacation." , budget-totalSum);
+
+            }else{
+                System.out.printf("%.2f leva needed." , totalSum-budget);
+            }
+        }
+    }
 }
