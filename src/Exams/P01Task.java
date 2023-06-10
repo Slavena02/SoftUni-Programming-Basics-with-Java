14b740bdfb98a5972e57e2be93a416f4fc3c53b3
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-10 22:12:32 +0300

    add

--- src/Exams/P01Task.java
+++ src/Exams/P01Task.java
@@ -7,5 +7,17 @@ public class P01Task {
         Scanner scanner = new Scanner(System.in);
 
 
+        String name = scanner.nextLine();
+        int season = Integer.parseInt(scanner.nextLine());
+        int episode = Integer.parseInt(scanner.nextLine());
+        double teamEpisode = Double.parseDouble(scanner.nextLine());
+
+        double advertisementEpisode = episode + (episode * 0.20);
+        double sumEpisode = episode + advertisementEpisode;
+        double specialEpisode = season * 10;
+        double totalSumMinutes = sumEpisode * episode * season + 30;
+        System.out.printf("Total time needed to watch the %s series is %.0f minutes.", name, totalSumMinutes);
+
+
     }
 }
