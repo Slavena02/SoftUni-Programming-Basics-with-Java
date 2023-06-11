f6293729b2077ec188ef743a23f53fb576d4bdc5
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-11 13:35:29 +0300

    add

--- src/Conditional_Statements_Advanced_MoreExercises/P09NumbersFrom1to10.java
+++ src/Conditional_Statements_Advanced_MoreExercises/P09NumbersFrom1to10.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class P09NumbersFrom1to10 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in);
-        for (int number = 1;number <= 100; number++){
+        for (int number = 1;number <= 10; number++){
             System.out.println(number);
 
         }
