91cc69ac4423518119089fbe4265676e0c079406
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-12 00:18:39 +0300

    add

--- src/WhileLoopMoreExercises/P02ReportSystem.java
+++ src/WhileLoopMoreExercises/P02ReportSystem.java
@@ -11,36 +11,45 @@ public class P02ReportSystem {
         int transaction = 0;
         double sumCard = 0.00;
         double sumMoney = 0.00;
+        double totalSum = 0;
+
+        int cash = 0;
+        int card = 0;
 
         String input = scanner.nextLine();
 
         while (!input.equals("End")) {
             int sum = Integer.parseInt(input);
-            if (sumMoney >= budget) {
-                double averagesCard = sumCard / transaction;
-                double averagesMoney = sumMoney / transaction;
-                System.out.printf("Average CS: %.2f", averagesMoney);
-                System.out.printf("Average CC: %.2f", averagesCard);
-            }
             transaction++;
 
             if (transaction % 2 == 0) {
                 if (sum < 10) {
-                    System.out.println("Error in transaction");
+                    System.out.println("Error in transaction!");
                 } else {
+                    card++;
                     sumCard += sum;
+                    totalSum += sum;
                     System.out.println("Product sold!");
                 }
 
             } else {
                 if (sum > 100) {
-                    System.out.println("Error in transaction");
+                    System.out.println("Error in transaction!");
                 } else {
+                    cash++;
                     sumMoney += sum;
+                    totalSum += sum;
                     System.out.println("Product sold!");
                 }
-                input = scanner.nextLine();
             }
+            if (totalSum >= budget) {
+                double averagesCard = sumCard / card;
+                double averagesMoney = sumMoney / cash;
+                System.out.printf("Average CS: %.2f%n", averagesMoney);
+                System.out.printf("Average CC: %.2f%n", averagesCard);
+            }
+                input = scanner.nextLine();
+
         }
         System.out.println("Failed to collect required money for charity.");
     }
