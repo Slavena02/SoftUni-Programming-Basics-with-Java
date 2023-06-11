90093b47f299cab31a97f5fc063153263c35bdab
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-11 20:01:01 +0300

    add

--- src/For_Loop_MoreExercises/P08EqualPairs.java
+++ src/For_Loop_MoreExercises/P08EqualPairs.java
@@ -6,5 +6,22 @@ public class P08EqualPairs {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int num = Integer.parseInt(scanner.nextLine());
+
+        int countNum = 0;
+        double sumNum = 0.00;
+        double sum1 = 0.00;
+
+        for (int i = 1; i <= num ; i++) {
+            int input1 = Integer.parseInt(scanner.nextLine());
+            int input2 = Integer.parseInt(scanner.nextLine());
+            sum1 = input1 + input2;
+
+
+
+
+
+            
+        }
     }
 }
---
+++ src/WhileLoopMoreExercises/P01Dishwasher.java
@@ -0,0 +1,4 @@
+package WhileLoopMoreExercises;
+
+public class P01Dishwasher {
+}
