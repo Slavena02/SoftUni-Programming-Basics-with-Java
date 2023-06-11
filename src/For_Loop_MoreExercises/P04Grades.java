5663deb0ac05a1c02d2f78e103c994192b07c446
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-11 17:35:53 +0300

    add

--- src/For_Loop_MoreExercises/P04Grades.java
+++ src/For_Loop_MoreExercises/P04Grades.java
@@ -8,10 +8,10 @@ public class P04Grades {
 
         int countStudent = Integer.parseInt(scanner.nextLine());
 
-        int count1 = 0;
-        int count2 = 0;
-        int count3 = 0;
-        int count4 = 0;
+        double count1 = 0;
+        double count2 = 0;
+        double count3 = 0;
+        double count4 = 0;
         double sumOfEstimates = 0.00;
 
         for (int i = 1; i <= countStudent ; i++) {
@@ -34,15 +34,15 @@ public class P04Grades {
         double arithmeticMean = sumOfEstimates / countStudent;
 
 
-        double topStudents = count4 /sumOfEstimates * 100;
-        double between4 = count3 /sumOfEstimates * 100;
-        double between3 = count2 /sumOfEstimates * 100;
-        double fail = count1 /sumOfEstimates * 100;
+        double topStudents = count4 /countStudent * 100;
+        double between4 = count3 /countStudent * 100;
+        double between3 = count2 /countStudent * 100;
+        double fail = count1 /countStudent * 100;
 
         System.out.printf("Top students: %.2f%%%n" , topStudents);
         System.out.printf("Between 4.00 and 4.99: %.2f%%%n", between4);
         System.out.printf("Between 3.00 and 3.99: %.2f%%%n",between3);
         System.out.printf("Fail: %.2f%%%n" , fail);
-        System.out.printf("Average: %.2f%%%n",arithmeticMean);
+        System.out.printf("Average: %.2f",arithmeticMean);
     }
 }
