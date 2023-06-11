33cd772cfd3f24ae150ab021ce5aee4deff8e6f2
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-11 13:16:41 +0300

    add

--- src/Conditional_Statements_Advanced_MoreExercises/P08PointOnRectangleBorder.java
+++ src/Conditional_Statements_Advanced_MoreExercises/P08PointOnRectangleBorder.java
@@ -6,6 +6,14 @@ public class P08PointOnRectangleBorder {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int x1 = Integer.parseInt(scanner.nextLine());
+        int x2 = Integer.parseInt(scanner.nextLine());
+        int y1 = Integer.parseInt(scanner.nextLine());
+        int y2 = Integer.parseInt(scanner.nextLine());
+        int x = Integer.parseInt(scanner.nextLine());
+        int y = Integer.parseInt(scanner.nextLine());
+
+
 
     }
 }
