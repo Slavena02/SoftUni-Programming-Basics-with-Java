8bc36ee1a3090acefe4c0565b43ad3c5407bb63b
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-11 16:56:31 +0300

    add

--- src/For_Loop_MoreExercises/P03Logistics.java
+++ src/For_Loop_MoreExercises/P03Logistics.java
@@ -11,21 +11,33 @@ public class P03Logistics {
         int bus = 0;
         int truck = 0;
         int train = 0;
-        double price = 0.00;
-        double kilograms = 0.00;
-        for (int i = 1; i <= count  ; i++) {
+
+        double t = 0.00;
+
+        for (int i = 1; i <= count; i++) {
             int input = Integer.parseInt(scanner.nextLine());
-            if (input<= 3){
-                bus += count;
-                price = bus * 200.00;
-            }else if (input <= 11){
-                truck += count;
-                price = truck * 175.00;
-            }else {
-                train += count;
-                price = train * 120.00;
+            if (input <= 3) {
+                t += input;
+                bus += input;
+            } else if (input <= 11) {
+                t += input;
+                truck += input;
+            } else {
+                t += input;
+                train += input;
             }
 
         }
+            double arithmeticMean = (bus * 200.00 + truck * 175.00 + train * 120.00) / t;
+
+            double busPercent = bus / t * 100;
+            double truckPercent = truck / t * 100;
+            double trainPercent = train / t * 100;
+
+            System.out.printf("%.2f%n" , arithmeticMean);
+            System.out.printf("%.2f%%%n", busPercent);
+            System.out.printf("%.2f%%%n",truckPercent);
+            System.out.printf("%.2f%%%n",trainPercent);
+        }
     }
-}
+
