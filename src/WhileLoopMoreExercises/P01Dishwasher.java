0576acde903faba69bb3d877484e68cc075d0aa6
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-11 20:39:08 +0300

    add

--- src/WhileLoopMoreExercises/P01Dishwasher.java
+++ src/WhileLoopMoreExercises/P01Dishwasher.java
@@ -9,36 +9,34 @@ public class P01Dishwasher {
 
         String input = scanner.nextLine();
 
-        double preparationMl = 0;
+        int newPreparation = 750;
+        int preparationMl = 0;
         int plates = 0;
         int countPlates = 0;
         int pots = 0;
         int countPots = 0;
         int period = 0;
-
-
-        while (!input.equals("End") ) {
-            int utensils = Integer.parseInt(input);
-             period++;
-            if(period % 3 == 0){
+        while (!input.equals("End")) {
+            int sumPreparation = Integer.parseInt(input);
+            newPreparation = sumPreparation * newPreparation;
+            int utensils = Integer.parseInt(scanner.nextLine());
+            period++;
+            if (period % 3 == 0) {
                 pots = utensils * 15;
                 preparationMl += pots;
                 countPots += utensils;
-            }else{
+            } else {
                 plates = utensils * 5;
                 preparationMl += plates;
                 countPlates += utensils;
             }
 
-            if (preparationMl <= 750){
-                System.out.println("Detergent was enough!");
-                System.out.printf("%d dishes and {брой чисти тенджери} pots were washed.");
-                System.out.printf("Leftover detergent {количество останал препарат} ml.");
+            if (preparationMl > newPreparation) {
+                System.out.printf("Not enough detergent, %d ml. more necessary!", preparationMl - newPreparation);
             }
-
-
-
-
         }
+        System.out.println("Detergent was enough!");
+        System.out.printf("%d dishes and %d pots were washed.", countPlates, countPots);
+        System.out.printf("Leftover detergent %d ml.", newPreparation - preparationMl);
     }
 }
