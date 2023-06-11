818620054997d9b05c87cea5ecb32ddc7c504108
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-11 16:58:25 +0300

    add

--- src/For_Loop_MoreExercises/P04Grades.java
+++ src/For_Loop_MoreExercises/P04Grades.java
@@ -1,4 +1,11 @@
 package For_Loop_MoreExercises;
 
+import java.util.Scanner;
+
 public class P04Grades {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
 }
