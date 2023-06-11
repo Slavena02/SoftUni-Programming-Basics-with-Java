d8a709c2dbb9ed2beedd75b996cf72f38784bcc3
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-12 00:25:53 +0300

    add

---
+++ src/WhileLoopMoreExercises/P03StreamOfLetters.java
@@ -0,0 +1,10 @@
+package WhileLoopMoreExercises;
+
+import java.util.Scanner;
+
+public class P03StreamOfLetters {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
