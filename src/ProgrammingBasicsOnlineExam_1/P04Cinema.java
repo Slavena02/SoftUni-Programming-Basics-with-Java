844e12c510682b5abe0979e64cd1f58f4664c632
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-12 23:37:40 +0300

    add

--- src/ProgrammingBasicsOnlineExam_1/P04Cinema.java
+++ src/ProgrammingBasicsOnlineExam_1/P04Cinema.java
@@ -6,20 +6,26 @@ public class P04Cinema {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String input = scanner.nextLine();
+      int n = Integer.parseInt(scanner.nextLine());
 
         double totalSum = 0.00;
         double priceTickets = 5.00;
         int countPeople = 0;
 
+        String input = scanner.nextLine();
+
         while (!input.equals("Movie time!")){
-            int people =Integer.parseInt(input);
+            int people =Integer.parseInt(scanner.nextLine());
             people++;
             double sum = people * priceTickets;
             if (people % 3 == 0){
                 sum = sum - 5.00;
             }
             totalSum  += sum;
+
+            if (people > n ){
+                break;
+            }
 
         }
     }
