896e41845d9b436ae2837291fc2ff1e9af5324f3
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-13 02:09:28 +0300

    add

--- src/ProgrammingBasicsOnlineExam_1/P05Series.java
+++ src/ProgrammingBasicsOnlineExam_1/P05Series.java
@@ -6,5 +6,48 @@ public class P05Series {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        double budget = Double.parseDouble(scanner.nextLine());
+        int countSerial = Integer.parseInt(scanner.nextLine());
+
+
+        double sum = 0.00;
+
+        for (int i = 1; i <=countSerial ; i++) {
+            String input = scanner.nextLine();
+            double price = Double.parseDouble(scanner.nextLine());
+            switch (input){
+                case "Thrones ":
+                    price -= price * 0.50;
+                    sum += price;
+                    break;
+                case "Lucifer ":
+                    price -= price * 0.40;
+                    sum += price;
+                    break;
+                case "Protector ":
+                    price -= price * 0.30;
+                    sum += price;
+                    break;
+                case "TotalDrama ":
+                    price -= price * 0.20;
+                    sum += price;
+                    break;
+                case "Area ":
+                    price -= price * 0.10;
+                    sum += price;
+                    break;
+            }
+            budget -= sum;
+            if (budget < sum){
+                break;
+            }
+            input = scanner.nextLine();
+        }
+        if (budget >= sum){
+            System.out.printf("You bought all the series and left with %.2f lv." , budget - sum);
+        }else {
+            System.out.printf("You need %.2f lv. more to buy the series!" , sum - budget);
+        }
+
     }
 }
