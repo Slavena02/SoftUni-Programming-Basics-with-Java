20cbf84fbfc74af619e87aa7f29e478ee0c2cf55
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-12 23:45:06 +0300

    add

--- src/ProgrammingBasicsOnlineExam_1/P04Cinema.java
+++ src/ProgrammingBasicsOnlineExam_1/P04Cinema.java
@@ -6,27 +6,30 @@ public class P04Cinema {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-      int n = Integer.parseInt(scanner.nextLine());
+        int n = Integer.parseInt(scanner.nextLine());
 
         double totalSum = 0.00;
         double priceTickets = 5.00;
         int countPeople = 0;
-
+        double sum = 0.00;
         String input = scanner.nextLine();
 
-        while (!input.equals("Movie time!")){
-            int people =Integer.parseInt(scanner.nextLine());
-            people++;
-            double sum = people * priceTickets;
-            if (people % 3 == 0){
+        while (!input.equals("Movie time!")) {
+            int people = Integer.parseInt(input);
+            countPeople += people;
+            sum = people * priceTickets;
+            if (people % 3 == 0) {
                 sum = sum - 5.00;
             }
-            totalSum  += sum;
+            totalSum += sum;
 
-            if (people > n ){
+            if (people > n) {
+                System.out.println("The cinema is full.");
                 break;
             }
-
+            input = scanner.nextLine();
         }
+        System.out.printf("There are %d seats left in the cinema.", n - countPeople);
+        System.out.printf("Cinema income - %.0f lv.", totalSum);
     }
 }
