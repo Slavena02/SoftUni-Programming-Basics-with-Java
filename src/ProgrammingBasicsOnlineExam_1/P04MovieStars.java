8b516e62683ec197ffdbe4ec407429953a807043
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-13 01:18:36 +0300

    add

--- src/ProgrammingBasicsOnlineExam_1/P04MovieStars.java
+++ src/ProgrammingBasicsOnlineExam_1/P04MovieStars.java
@@ -7,5 +7,30 @@ public class P04MovieStars {
         Scanner scanner = new Scanner(System.in);
 
 
+        double budgetActor = Double.parseDouble(scanner.nextLine());
+        String input = scanner.nextLine();
+
+        while (!input.equals("ACTION")){
+            int length = input.length();
+            if (length > 15){
+                budgetActor -= budgetActor * 0.20;
+                input = scanner.nextLine();
+                continue;
+            }
+
+            double many = Double.parseDouble(scanner.nextLine());
+            budgetActor -= many;
+
+         input = scanner.nextLine();
+
+
+         if (budgetActor >= many){
+             System.out.printf("We are left with %.2f leva.", budgetActor - many);
+         }else {
+             System.out.printf("We need %.2f leva for our actors." , many - budgetActor);
+         }
+        }
+
+
     }
 }
