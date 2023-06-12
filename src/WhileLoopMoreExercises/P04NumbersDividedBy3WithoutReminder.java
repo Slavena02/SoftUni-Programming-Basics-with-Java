c9a6982e2fd19aea1b24c3bda7ea30353864d4b6
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-12 23:21:33 +0300

    add

--- src/WhileLoopMoreExercises/P04NumbersDividedBy3WithoutReminder.java
+++ src/WhileLoopMoreExercises/P04NumbersDividedBy3WithoutReminder.java
@@ -6,5 +6,9 @@ public class P04NumbersDividedBy3WithoutReminder {
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in);
 
+        for (int i = 1; i <=100; i+=3) {
+            System.out.println(i);
+
+        }
     }
 }
