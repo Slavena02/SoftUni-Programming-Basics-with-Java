7e4ac2c4452d8ed7cca290d5d9b4b5d88663a2ab
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-14 00:45:46 +0300

    add

---
+++ src/ProgrammingBasicsOnlineExam_1/P04Club.java
@@ -0,0 +1,10 @@
+package ProgrammingBasicsOnlineExam_1;
+
+import java.util.Scanner;
+
+public class P04Club {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
