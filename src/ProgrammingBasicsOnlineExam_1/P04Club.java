4ae9b7c87776bc52ecb0dd6626c3b5641d5d1c3f
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-14 00:59:28 +0300

    add

--- src/ProgrammingBasicsOnlineExam_1/P04Club.java
+++ src/ProgrammingBasicsOnlineExam_1/P04Club.java
@@ -6,5 +6,33 @@ public class P04Club {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        double sum = Double.parseDouble(scanner.nextLine());
+
+        double price = 0.00;
+        double sumPrice = 0.00;
+        String input = scanner.nextLine();
+        while (!input.equals("Party!")) {
+            int count = Integer.parseInt(scanner.nextLine());
+            int length = input.length();
+
+            price += length;
+            sumPrice = price * count;
+            if (sumPrice % 2 == 1) {
+                sumPrice -= sumPrice * 0.25;
+            }
+
+            if (sumPrice >= sum) {
+                break;
+            }
+            input = scanner.nextLine();
+        }
+        if ( sumPrice >= sum) {
+            System.out.println("Target acquired.");
+            System.out.printf("Club income - %.2f leva.", sumPrice);
+        }else {
+            System.out.printf(" We need %.2f leva more.%n" , sum - sumPrice);
+            System.out.printf("Club income - %.2f leva.", sumPrice);
+        }
+
     }
 }
