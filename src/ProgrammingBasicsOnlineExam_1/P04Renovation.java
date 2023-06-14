44e3fba4b792ec7af9aeefaab8fafdc06030a182
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-14 22:55:13 +0300

    add

--- src/ProgrammingBasicsOnlineExam_1/P04Renovation.java
+++ src/ProgrammingBasicsOnlineExam_1/P04Renovation.java
@@ -5,6 +5,15 @@ import java.util.Scanner;
 public class P04Renovation {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        int h = Integer.parseInt(scanner.nextLine());
+        int width = Integer.parseInt(scanner.nextLine());
+        int percent = Integer.parseInt(scanner.nextLine());
+
+        String input = scanner.nextLine();
+        while (!input.equals("")){
+
+        }
         
     }
 }
