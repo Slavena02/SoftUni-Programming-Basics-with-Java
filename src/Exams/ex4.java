9217aa1dff9769b66356d4cf7e15256132a26e82
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-17 22:22:18 +0300

    add

--- src/Exams/ex4.java
+++ src/Exams/ex4.java
@@ -7,5 +7,31 @@ public class ex4 {
         Scanner scanner = new Scanner(System.in);
 
 
+        int countCat = Integer.parseInt(scanner.nextLine());
+
+        int group1 = 0;
+        int group2 = 0;
+        int group3 = 0;
+        int totalGrams = 0;
+
+        for (int i = 1; i <=countCat ; i++) {
+            int gramsFood = Integer.parseInt(scanner.nextLine());
+             totalGrams += gramsFood;
+            if (gramsFood >=100 && gramsFood < 200){
+                group1 ++;
+            }else if (gramsFood >= 200 && gramsFood < 300){
+                group2++;
+            }else if (gramsFood >= 300 && gramsFood <= 400){
+                group3++;
+            }
+        }
+
+        double totalKg = totalGrams / 1000.00;
+        double totalSumFood = totalKg * 12.45;
+
+        System.out.printf("Group 1: %d cats.%n" , group1);
+        System.out.printf("Group 2: %d cats.%n" , group2);
+        System.out.printf("Group 3: %d cats.%n" , group3);
+        System.out.printf("Price for food per day: %.2f lv." , totalSumFood);
     }
 }
