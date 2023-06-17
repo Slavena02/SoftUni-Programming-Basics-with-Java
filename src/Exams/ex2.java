3b06956934471f6709d803f761f3f84673927257
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-17 21:46:25 +0300

    add

--- src/Exams/ex2.java
+++ src/Exams/ex2.java
@@ -6,5 +6,22 @@ public class ex2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-    }
-}
+                String fanName = scanner.nextLine();
+                double budget = Double.parseDouble(scanner.nextLine());
+                int beerQuantity = Integer.parseInt(scanner.nextLine());
+                int chipsQuantity = Integer.parseInt(scanner.nextLine());
+
+                double beerPrice = 1.20 * beerQuantity;
+                double chipsPrice = beerPrice - (beerPrice * 0.55);
+                double totalSumChips = Math.ceil(chipsPrice * chipsQuantity);
+
+                double totalCost = beerPrice + totalSumChips;
+
+                if (budget >= totalCost) {
+                    System.out.printf("%s bought a snack and has %.2f leva left.", fanName, budget - totalCost);
+                } else {
+                    System.out.printf("%s needs %.2f more leva!", fanName, totalCost - budget);
+                }
+            }
+        }
+
