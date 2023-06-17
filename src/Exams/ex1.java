706044d6b34b6c7cab52e786d5dba0d30b2c0d55
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-17 21:36:53 +0300

    add

--- src/Exams/ex1.java
+++ src/Exams/ex1.java
@@ -6,5 +6,26 @@ public class ex1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-    }
-}
+                double pagePrintingPrice = Double.parseDouble(scanner.nextLine());
+                double coverPrintingPrice = Double.parseDouble(scanner.nextLine());
+                int paperDiscount = Integer.parseInt(scanner.nextLine());
+                double designerFee = Double.parseDouble(scanner.nextLine());
+                int teamPaymentPercentage = Integer.parseInt(scanner.nextLine());
+
+                int totalPages = 899;
+                int totalCovers = 2;
+
+                double pagesCost = pagePrintingPrice * totalPages;
+                double coversCost = coverPrintingPrice * totalCovers;
+
+                double paperDiscountPercentage = (100 - paperDiscount) / 100.0;
+                double totalPrintingCost = (pagesCost + coversCost) * paperDiscountPercentage;
+
+                double designerPayment = totalPrintingCost + designerFee;
+
+                double teamPaymentPercentageDecimal = teamPaymentPercentage / 100.0;
+                double avtonomPayment = designerPayment * (1 - teamPaymentPercentageDecimal);
+
+                System.out.printf("Avtonom should pay %.2f BGN.", avtonomPayment);
+            }
+        }
