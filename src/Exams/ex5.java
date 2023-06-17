c703545830582a25f02603e0d954630beff90211
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-17 22:38:36 +0300

    add

--- src/Exams/ex5.java
+++ src/Exams/ex5.java
@@ -6,5 +6,31 @@ public class ex5 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String input = scanner.nextLine();
+
+        int child = 0;
+        int mature = 0;
+
+        double toys = 5.00;
+        double pullover = 15.00;
+
+        while (!input.equals("Christmas")){
+            int age = Integer.parseInt(input);
+            if ( age <= 16){
+          child++;
+            }else {
+                mature++;
+            }
+            input = scanner.nextLine();
+        }
+
+        double childToysPrice = child * toys;
+        double maturePulloverPrice = mature * pullover;
+
+        System.out.printf("Number of adults: %d%n" , mature);
+        System.out.printf("Number of kids: %d%n" , child);
+        System.out.printf("Money for toys: %.0f%n" , childToysPrice);
+        System.out.printf("Money for sweaters: %.0f" , maturePulloverPrice);
+
     }
 }
