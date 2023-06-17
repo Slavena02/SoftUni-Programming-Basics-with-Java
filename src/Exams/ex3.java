73c8ccf4112d304e739eeeace151235797e9815d
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-17 22:01:22 +0300

    add

--- src/Exams/ex3.java
+++ src/Exams/ex3.java
@@ -6,5 +6,47 @@ public class ex3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String cake = scanner.nextLine();
+        int count = Integer.parseInt(scanner.nextLine());
+        int dayOfMonth = Integer.parseInt(scanner.nextLine());
+
+        double priceProduct = 0.00;
+
+        switch (cake) {
+            case "Cake":
+                if (dayOfMonth <= 15){
+                    priceProduct = count * 24.00;
+                }else {
+                    priceProduct = count * 28.70;
+                }
+                break;
+            case "Souffle":
+                if (dayOfMonth <= 15){
+                    priceProduct = count * 6.66;
+                }else {
+                    priceProduct = count * 9.80;
+                }
+                break;
+            case "Baklava":
+                if (dayOfMonth <= 15){
+                    priceProduct = count * 12.60;
+                }else {
+                    priceProduct = count * 16.98;
+                }
+                break;
+        }
+        if (dayOfMonth <= 22){
+            if (priceProduct >= 100 || priceProduct < 200){
+                priceProduct -= priceProduct * 0.15;
+            }else {
+                priceProduct -= priceProduct * 0.25;
+            }
+        }
+
+        if ( dayOfMonth <= 15){
+            priceProduct -= priceProduct * 0.10;
+        }
+
+        System.out.printf("%.2f" , priceProduct);
     }
 }
