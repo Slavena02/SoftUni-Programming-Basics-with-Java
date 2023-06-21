49ec54ae43def41b9abe347550ab6f6719f5e158
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-21 22:32:33 +0300

    add

---
+++ src/More/P04BackIn30Minutes.java
@@ -0,0 +1,10 @@
+package More;
+
+import java.util.Scanner;
+
+public class P04BackIn30Minutes {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
