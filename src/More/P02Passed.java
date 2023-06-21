0ebfb57601fc24d60559835ce0e9703625c27735
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-21 22:26:54 +0300

    add

--- src/More/P02Passed.java
+++ src/More/P02Passed.java
@@ -6,5 +6,10 @@ public class P02Passed {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        double grade = Double.parseDouble(scanner.nextLine());
+
+        if ( grade >= 3){
+            System.out.println("Passed!");
+        }
     }
 }
