1bcc3ffc32078c67371addc65c4c6ee83cb35f8b
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-21 22:30:38 +0300

    add

--- src/More/P03PassedOrFailed.java
+++ src/More/P03PassedOrFailed.java
@@ -6,5 +6,12 @@ public class P03PassedOrFailed {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        double grade = Double.parseDouble(scanner.nextLine());
+
+        if (grade >= 3){
+            System.out.println("Passed!");
+        }else{
+            System.out.println("Failed!");
+        }
     }
 }
