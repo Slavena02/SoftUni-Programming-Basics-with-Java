d6ec47bdfd84ccc7bcf89c1a41c2c20e9f8522c1
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-21 22:28:19 +0300

    add

---
+++ src/More/P03PassedOrFailed.java
@@ -0,0 +1,10 @@
+package More;
+
+import java.util.Scanner;
+
+public class P03PassedOrFailed {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
