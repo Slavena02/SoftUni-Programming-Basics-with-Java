8b420d6902f14f1c858895aac870080ddf0ba315
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-25 23:09:02 +0300

    add

--- src/More/P01StudentInformation.java
+++ src/More/P01StudentInformation.java
@@ -7,7 +7,9 @@ public class P01StudentInformation {
         Scanner scanner = new Scanner(System.in);
 
         String name = scanner.nextLine();
+
         int age =  Integer.parseInt(scanner.nextLine());
+
         double grade =  Double.parseDouble(scanner.nextLine());
 
 
