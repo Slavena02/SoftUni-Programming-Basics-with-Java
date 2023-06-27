4598ab521958dab7045c81d8556be6df17663fee
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-27 23:48:59 +0300

    add

---
+++ src/Demo/demo1.java
@@ -0,0 +1,12 @@
+package Demo;
+
+import java.util.Scanner;
+
+public class demo1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+
+        int[][] myNumbers = {{1,2,3,4} , {5,6,7,8,9,10} };
+        System.out.println(myNumbers[0][3]);
+    }
+}
