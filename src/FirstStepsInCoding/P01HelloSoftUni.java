b3e9b8c36cc2a4a85c3feab0e7af362fe68b4f9e
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-27 22:46:01 +0300

    add

--- src/FirstStepsInCoding/P01HelloSoftUni.java
+++ src/FirstStepsInCoding/P01HelloSoftUni.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class P01HelloSoftUni {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Hello SoftUni");
 
 
