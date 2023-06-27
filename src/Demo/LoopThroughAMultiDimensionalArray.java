69a0b7700f873ba33c4e029d29399885b4c82201
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-28 00:05:20 +0300

    add

--- src/Demo/LoopThroughAMultiDimensionalArray.java
+++ src/Demo/LoopThroughAMultiDimensionalArray.java
@@ -6,5 +6,11 @@ public class LoopThroughAMultiDimensionalArray {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int [][] myNumbers = {{1,2,3,4,5},{6,7,8,9,10}};
+        for (int i = 0 ; i < myNumbers.length; ++i){
+            for (int j = 0; j < myNumbers[i].length ; ++j) {
+
+            }
+        }
     }
 }
