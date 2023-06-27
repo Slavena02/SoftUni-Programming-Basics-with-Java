79ab998d2c49bb4c778c118fb827de4c4ab78896
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-27 23:32:05 +0300

    add

--- src/Demo/demo.java
+++ src/Demo/demo.java
@@ -8,8 +8,6 @@ public class demo {
 
         String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
         for (int i = 0; i < cars.length; i++) {
-
-
             System.out.println(cars[i]);
         }
 
