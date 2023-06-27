9cc468740c9b98a1551608190c2c14240471f480
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-28 00:02:20 +0300

    add

---
+++ src/Demo/LoopThroughAMultiDimensionalArray.java
@@ -0,0 +1,10 @@
+package Demo;
+
+import java.util.Scanner;
+
+public class LoopThroughAMultiDimensionalArray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
