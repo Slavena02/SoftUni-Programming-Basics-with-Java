e52bdc13b172c7f6536e045e1d20c17936d3baf0
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-27 23:30:59 +0300

    add

---
+++ src/Demo/demo.java
@@ -0,0 +1,14 @@
+package Demo;
+
+import java.util.Scanner;
+
+public class demo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in );
+
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        for (int i = 0; i < cars.length; i++) {
+            System.out.println(cars[i]);
+        }
+    }
+}
