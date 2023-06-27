73b93f6accbfedc95ec135ab53b4ed238bf337a9
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-27 23:54:44 +0300

    add

---
+++ src/Demo/ChangeElementValues.java
@@ -0,0 +1,9 @@
+package Demo;
+
+import java.util.Scanner;
+
+public class ChangeElementValues {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+    }
+}
