eeb54baea099279192276a1761fc59114b5c011b
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-27 23:56:16 +0300

    add

--- src/Demo/ChangeElementValues.java
+++ src/Demo/ChangeElementValues.java
@@ -5,5 +5,8 @@ import java.util.Scanner;
 public class ChangeElementValues {
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in);
+        int [][] myNumbers = {{1,2,3,4,5,},{6,7,8,9,10}};
+        myNumbers[1][1] = 3;
+        System.out.println(myNumbers[1][1]);
     }
 }
