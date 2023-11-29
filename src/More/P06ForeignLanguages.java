412609203b4a6f19c17bfcd6bd44c990819ccd4b
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-11-29 22:13:24 +0200

    add

---
+++ src/More/P06ForeignLanguages.java
@@ -0,0 +1,10 @@
+package More;
+
+import java.util.Scanner;
+
+public class P06ForeignLanguages {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
