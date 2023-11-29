4387176133165a7698d46eddcc9e8b8bfd352aed
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-11-29 22:57:14 +0200

    add

--- src/More/P07TheatrePromotion.java
+++ src/More/P07TheatrePromotion.java
@@ -1,34 +1,31 @@
 package More;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class P07TheatrePromotion {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String day = scanner.nextLine();
+        String day = scanner.nextLine().toLowerCase();
         int age = Integer.parseInt(scanner.nextLine());
 
         double price  = 0;
 
         switch (day) {
             case "Weekday":
-                if (0 <= age && age <= 18) {
+                if (0 <= age && age <= 18||age > 64 && age <= 122 ){
                     price = 12;
                 } else if (age > 18 && age <= 64) {
                     price = 18;
-                } else if (age > 64 && age <= 122) {
-                    price = 12;
                 }else{
                     System.out.println("Error");
                 }
             case "Weekend":
-                if (0 <= age && age <= 18) {
+                if (0 <= age && age <= 18 || age > 64 && age <= 122){
                     price = 15;
                 } else if (age > 18 && age <= 64) {
                     price = 20;
-                } else if (age > 64 && age <= 122) {
-                    price = 15;
                 }else{
                     System.out.println("Error");
                 }
