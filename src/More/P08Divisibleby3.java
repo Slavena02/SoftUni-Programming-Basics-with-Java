fd6897a743cf93dcc5fde73a05298b5982bd6b7d
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-11-29 23:29:22 +0200

    add

---
+++ src/More/P08Divisibleby3.java
@@ -0,0 +1,13 @@
+package More;
+
+import java.util.Scanner;
+
+public class P08Divisibleby3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        for (int num = 3 ; num <= 100; num +=3){
+            System.out.println(num);
+        }
+    }
+}
