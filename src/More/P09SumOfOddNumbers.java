1a58f390c23c84eb4e9fdb1de881b7ed34db3a30
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-11-29 23:59:54 +0200

    add

--- src/More/P09SumOfOddNumbers.java
+++ src/More/P09SumOfOddNumbers.java
@@ -6,10 +6,16 @@ public class P09SumOfOddNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int num = Integer.parseInt(scanner.nextLine());
+
         int sum = 0;
-        for (int num = 1; num <= 100; num +=3){
-            System.out.println(num);
-            sum += num;
+
+        for (int num1 = 1; num1 <= num; num1++) {
+
+            if (num1 % 2 == 1) {
+                sum += num1;
+                System.out.println(num1);
+            }
         }
         System.out.println(sum);
     }
