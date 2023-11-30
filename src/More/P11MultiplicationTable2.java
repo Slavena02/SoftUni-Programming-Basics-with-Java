fa1e2fbd8622e79c1dd6f942ca8add275cb32061
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-11-30 22:59:34 +0200

    add new ex

--- src/More/P11MultiplicationTable2.java
+++ src/More/P11MultiplicationTable2.java
@@ -1,4 +1,11 @@
 package More;
 
+import java.util.Scanner;
+
 public class P11MultiplicationTable2 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+    }
 }
