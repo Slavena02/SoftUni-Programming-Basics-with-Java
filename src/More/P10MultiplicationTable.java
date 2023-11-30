09885e915cc4b0e251a80c55d05aa31bfa50b0f8
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-11-30 22:38:53 +0200

    add

---
+++ src/More/P10MultiplicationTable.java
@@ -0,0 +1,10 @@
+package More;
+
+import java.util.Scanner;
+
+public class P10MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner scanner =  new Scanner(System.in);
+
+    }
+}
