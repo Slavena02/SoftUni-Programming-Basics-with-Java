802b4315617c851469e17d6a21d42fb3739ac490
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-11-30 23:12:16 +0200

    add new ex

--- src/More/P11MultiplicationTable2.java
+++ src/More/P11MultiplicationTable2.java
@@ -7,5 +7,15 @@ public class P11MultiplicationTable2 {
 
         Scanner scanner = new Scanner(System.in);
 
+        int num1 = Integer.parseInt(scanner.nextLine());
+        int num2 = Integer.parseInt(scanner.nextLine());
+
+        int sum = 0 ;
+        for (int i = 1; i <=num1 ; i++) {
+            for (int j = 1; j <=num2 ; j++) {
+             sum = num1 * num2;
+            }
+        }
+        System.out.println(num1 + " X " + num2 + " = " + sum);
     }
 }
