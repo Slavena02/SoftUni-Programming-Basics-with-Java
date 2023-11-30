492c8fffdd026ec2335bcd096c6c2bbefaeb988b
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-11-30 23:50:29 +0200

    add new ex

--- src/More/P12EvenNumber.java
+++ src/More/P12EvenNumber.java
@@ -1,4 +1,20 @@
 package More;
 
+import java.util.Scanner;
+
 public class P12EvenNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 1 ; i <=  number ; i++) {
+            if(number % 2 == 1){
+                System.out.println("Please write an even number.");
+            }else{
+                System.out.printf("The number is: %d" , number);
+            }
+
+        }
+    }
 }
