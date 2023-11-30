1222dfdfb9670cb0e7b88b17d41a3073c3019456
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-11-30 22:53:00 +0200

    add

--- src/More/P10MultiplicationTable.java
+++ src/More/P10MultiplicationTable.java
@@ -4,7 +4,15 @@ import java.util.Scanner;
 
 public class P10MultiplicationTable {
     public static void main(String[] args) {
-        Scanner scanner =  new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
+        int num = Integer.parseInt(scanner.nextLine());
+
+        int sum = 0;
+
+            for (int num1 =1 ; num1 <= 10 ; num1++){
+                sum = num * num1;
+                System.out.printf("%d X %d = %d" , num , num1 , sum);
+            }
+        }
     }
-}
